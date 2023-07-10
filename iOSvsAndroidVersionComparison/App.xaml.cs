b1590ccaef86de17433667d58790708bf9a1ad33
Brian J Ball <myotherpants@gmail.com>
2023-07-10 14:55:51 -0400

    Initial Commit

---
+++ iOSvsAndroidVersionComparison/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace iOSvsAndroidVersionComparison;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ iOSvsAndroidVersionComparison/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace iOSvsAndroidVersionComparison;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
